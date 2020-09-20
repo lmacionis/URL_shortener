cc2acafb82f524bbb826316553cdaf6425cb59ff
Lukas <lmacionis@yahoo.com>
2020-09-20 18:26:43 +0300

    changed method setUpTestData to setUp and added test test_saving_and_retrieving_url_items

--- short_url/url/tests/test_models.py
+++ short_url/url/tests/test_models.py
@@ -3,9 +3,17 @@ from url.models import AllUrl
 
 
 class AllUrlTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        AllUrl.objects.create(long_url="www.google.com", short_url="kirrgo")
+
+    def setUp(self):
+        first_url = AllUrl()
+        first_url.long_url = 'www.google.com'
+        first_url.short_url = '123456'
+        first_url.save()
+
+        second_url = AllUrl()
+        second_url.long_url = 'www.yahoo.com'
+        second_url.short_url = '789456'
+        second_url.save()
 
     def test_long_url_label(self):
         url = AllUrl.objects.get(id=1)
@@ -26,3 +34,14 @@ class AllUrlTest(TestCase):
         url = AllUrl.objects.get(id=1)
         max_length = url._meta.get_field('short_url').max_length
         self.assertEquals(max_length, 6)
+
+    def test_saving_and_retrieving_url_items(self):
+        saved_urls = AllUrl.objects.all()
+        self.assertEqual(saved_urls.count(), 2)
+
+        first_saved_url = saved_urls[0]
+        second_saved_url = saved_urls[1]
+        self.assertEqual(first_saved_url.short_url, '123456')
+        self.assertEqual(first_saved_url.long_url, 'www.google.com')
+        self.assertEqual(second_saved_url.short_url, '789456')
+        self.assertEqual(second_saved_url.long_url, 'www.yahoo.com')
